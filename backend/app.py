396dd4270a26aff78fa950bf73cc8405e41eda30
Lakshya Jain <lakshyajain579@gmail.com>
2026-04-09 01:07:15 +0530

    fixed render port issue

--- backend/app.py
+++ backend/app.py
@@ -36,5 +36,8 @@ def home():
     return {"message": "Backend + Local MongoDB connected"}
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
